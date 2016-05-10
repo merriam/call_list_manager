f5c10fdb2f9868482bfe0ce7aabb2fcb23f8f497
Charles Merriam <charles.merriam@gmail.com>
2016-05-09 19:52:33 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple call list application for a two hour hack
+at Prototypes Night and DevBootCamp offices on May 9, 2016.
+
+Working with Anish Kothari
+
---
+++ data.py
@@ -0,0 +1,23 @@
+""" Data as a simple array of hashes
+"""
+
+data = [
+  {
+    'first': 'Charles',
+    'last' : 'Merriam',
+    'phone': '408.368.6050',
+    'talked_about': 'Chatting about Bootstrap.  Need answer on Flask.',
+    'identity_notes': 'Guy from prototype nights',
+    'interval': 'Week',
+    'last_call': '2016-01-15'
+  },
+  {
+    'first': 'Anish',
+    'last': 'Kothari',
+    'phone': '205.555.1212',
+    'talked_about': 'Flask.  Bootstrap.  Call lists',
+    'identity_notes': 'Guy from prototype nights',
+    'interval': 'Month',
+    'last_call':'2016-05-15'
+  }
+]
